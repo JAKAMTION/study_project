ffccbb6ca2c005ff64be7be19827911ddd86c1ca
JAKAMTION <j18829852620@163.com>
2024-08-13 21:43:33 +0800

    修改

--- RemoteCtrl/RemoteClient/RemoteClient.cpp
+++ RemoteCtrl/RemoteClient/RemoteClient.cpp
@@ -24,7 +24,7 @@ END_MESSAGE_MAP()
 CRemoteClientApp::CRemoteClientApp()
 {
 	// 支持重新启动管理器
-	//
+	//123
 	m_dwRestartManagerSupportFlags = AFX_RESTART_MANAGER_SUPPORT_RESTART;
 
 	// TODO: 在此处添加构造代码，
